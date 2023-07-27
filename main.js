745596ba4c703101b4d3c34c82e81667f015c96b
SofiaDav06 <115194975+SofiaDav06@users.noreply.github.com>
2023-07-27 07:45:51 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const color = document.querySelector("#color")
+const salida = document.querySelector("#salida")
+
+
+// console.log(color.value)
+
+//Eventos
+color.addEventListener("input", () => {
+    console.log(color.value)
+    salida.innerHTML = color.value
+    salida.style.background = color.value
+    salida.style.color = "#FFF"
+})
